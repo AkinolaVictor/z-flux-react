62916a08439a1efd7fb58cf2647f6113e0193c9c
Boughous-startup <akinolavictor26@gmail.com>
2026-07-22 02:25:18 +0100

    'updated...'

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { components } from "./components"
 import { text_components } from "./components/texts"
-// import {components} from "./components"
 
 export function justSayHello({name}: {name: string}){
     console.log("Hey!!! Hello ", name, ". Package is updated!!!")
 } 
 
 export const {
-    // TextAnimationTemplate, 
-    // TextColorFade,
-    // TextRevealByHeight,
-
     TextEngine,
     TextFade,
     TextFadeDown,
